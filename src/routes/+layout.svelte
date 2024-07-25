00072d4bc52302c74c381bc3835745fcb5f6e1c3
Max <46341774+Max-Soze@users.noreply.github.com>
2024-07-25 18:30:09 +0100

    changed background to be dark

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,16 @@
 
 <Navbar />
 <slot />
+
+<style>
+    :root {
+        --mainColor: #363a41;
+        --textColor: #ffffff;
+        --inputColor: #4f5662;
+        --accentColor: #00904b;
+    }
+
+    :global(html) {
+        background-color: #26292f;
+    }
+</style>
