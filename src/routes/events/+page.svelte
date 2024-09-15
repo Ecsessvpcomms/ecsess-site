dc93d6396306306a6b13f63dfc8b38afef51d124
Charles Blancas <86479293+charlesblancas@users.noreply.github.com>
2024-09-14 20:38:55 -0400

    #3 Events page with fake data

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Event from './Event.svelte';
+	export let data;
+</script>
+
+<h1 class="text-5xl text-center">All Upcoming Events</h1>
+
+<div class="flex justify-center items-center min-h-screen">
+	<div class="flex flex-col gap-4 w-3/5 justify-center items-center p-10">
+		{#each data.posts as post}
+			<Event {...post} />
+		{/each}
+	</div>
+</div>
---
+++ src/routes/events/Event.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let image, title, date, location, information, ticketsLink;
+</script>
+
+<div class="rounded overflow-hidden shadow-lg flex flex-row bg-green-700 h-96">
+	<img class="w-1/2 h-full object-cover" src={image} alt={'Picture for ' + title} />
+	<div class="flex flex-col px-6 py-4 text-white w-1/2">
+		<div class="font-extrabold text-xl mb-2">{title}</div>
+		<div class="font-semibold text-l">{date}</div>
+		<div class="font-semibold text-l mb-2">{location}</div>
+		<p class="text-base flex-grow mb-4 overflow-y-auto">
+			{information}
+		</p>
+
+		<div class="flex justify-center">
+			<button
+				class="bg-green-800 rounded px-8 py-2 font-semibold"
+				on:click={() => (window.location = ticketsLink)}>Get Tickets</button
+			>
+		</div>
+	</div>
+</div>
