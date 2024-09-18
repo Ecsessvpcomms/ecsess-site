82ad8ec3718cade7e0b4715f3ffb5c26b6296155
Max <46341774+Max-Soze@users.noreply.github.com>
2024-09-18 03:28:33 -0400

    fixed image loading bug

--- src/routes/events/+page.ts
+++ src/routes/events/+page.ts
@@ -2,6 +2,6 @@ import client from "$lib/sanityClient"
 import type { PageLoad } from "./$types"
 
 export async function load () {
-    const events = await client.fetch("*[_type == 'event'] {title, start, end, location, description, image->}");
+    const events = await client.fetch("*[_type == 'event'] {title, start, end, location, description, image}");
     return { events };
 }
--- src/routes/events/Event.svelte
+++ src/routes/events/Event.svelte
@@ -1,9 +1,12 @@
 <script>
 	export let title, start, end, location, description, image;
+	import client from "$lib/sanityClient"
+	import imageUrlBuilder from "@sanity/image-url";
+	const builder = imageUrlBuilder(client)
 </script>
 
 <div class="rounded overflow-hidden shadow-lg flex flex-row bg-green-700 h-96">
-	<img class="w-1/2 h-full object-cover" src={image} alt={'Picture for ' + title} />
+	<img class="w-1/2 h-full object-cover" src={builder.image(image).url()} alt={'Picture for ' + title} />
 	<div class="flex flex-col px-6 py-4 text-white w-1/2">
 		<div class="font-extrabold text-xl mb-2">{title}</div>
 		<div class="font-semibold text-l">{start}</div>
