a25722ef85b197a05a7ef8b54c311655fb86cd8a
Max <46341774+Max-Soze@users.noreply.github.com>
2024-09-18 03:14:37 -0400

    svelte reads from sanity

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,14 +1,15 @@
 <script>
 	import Event from './Event.svelte';
 	export let data;
+	console.log(data);
 </script>
 
 <h1 class="text-5xl text-center">All Upcoming Events</h1>
 
 <div class="flex justify-center items-center min-h-screen">
 	<div class="flex flex-col gap-4 w-3/5 justify-center items-center p-10">
-		{#each data.posts as post}
-			<Event {...post} />
+		{#each data.events as event}
+			<Event {...event} />
 		{/each}
 	</div>
 </div>
--- src/routes/events/Event.svelte
+++ src/routes/events/Event.svelte
@@ -1,21 +1,21 @@
 <script>
-	export let image, title, date, location, information, ticketsLink;
+	export let title, start, end, location, description, image;
 </script>
 
 <div class="rounded overflow-hidden shadow-lg flex flex-row bg-green-700 h-96">
 	<img class="w-1/2 h-full object-cover" src={image} alt={'Picture for ' + title} />
 	<div class="flex flex-col px-6 py-4 text-white w-1/2">
 		<div class="font-extrabold text-xl mb-2">{title}</div>
-		<div class="font-semibold text-l">{date}</div>
+		<div class="font-semibold text-l">{start}</div>
 		<div class="font-semibold text-l mb-2">{location}</div>
 		<p class="text-base flex-grow mb-4 overflow-y-auto">
-			{information}
+			{description}
 		</p>
 
 		<div class="flex justify-center">
 			<button
 				class="bg-green-800 rounded px-8 py-2 font-semibold"
-				on:click={() => (window.location = ticketsLink)}>Get Tickets</button
+				on:click={() => (/*window.location = ticketsLink*/console.log("Been pressed"))}>Get Tickets</button
 			>
 		</div>
 	</div>
